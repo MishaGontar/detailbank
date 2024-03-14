9fe39b71bef97dafb27cf16db8ddcc68d95086a8
MishaGontar <turictscol40@gmail.com>
2024-03-14 16:11:55 +0200

    test

---
+++ src/test/java/guryme/detailbank/DetailbankApplicationTests.java
@@ -0,0 +1,13 @@
+package guryme.detailbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
