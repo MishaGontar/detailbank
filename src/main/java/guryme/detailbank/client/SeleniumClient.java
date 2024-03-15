ce8eee6db58c9bf3839adaf50b2d6d277ac76de6
MishaGontar <turictscol40@gmail.com>
2024-03-15 22:32:34 +0200

    resolve

--- src/main/java/guryme/detailbank/client/SeleniumClient.java
+++ src/main/java/guryme/detailbank/client/SeleniumClient.java
@@ -2,7 +2,7 @@ package guryme.detailbank.client;
 
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,10 +16,10 @@ public class SeleniumClient {
     WebDriverWait wait;
 
     public SeleniumClient() {
-        DesiredCapabilities dc = new DesiredCapabilities();
-        dc.setBrowserName("firefox");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--headless");
         try {
-            driver = new RemoteWebDriver(new URL("http://firefox:4444/wd/hub/"), dc);
+            driver = new RemoteWebDriver(new URL("http://firefox:4444/wd/hub/"), firefoxOptions);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
